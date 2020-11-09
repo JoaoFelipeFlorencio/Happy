3406bccb3388b44fecaa23a58ca33ff0f20c551a
JoaoFelipeFlorencio <florenciojoaofelipe@gmail.com>
2020-11-08 22:57:52 -0300

    Day 2 part 4

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -19,3 +19,21 @@ const icon = L.icon({
 
 // create and add marker
 L.marker([-8.1268417, -34.9357666], { icon }).addTo(map);
+
+/*Image gallery*/
+
+function selectImage(event) {
+  const button = event.currentTarget;
+  // remover todas as classes active
+  const buttons = document.querySelectorAll(".images button");
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+  //selecionar a imagem clicada
+  const image = button.children[0]
+  const imageContainer = document.querySelector(".orphanage-details > img")
+  //atualizar o container de imagem
+  imageContainer.src= image.src
+  //adicionar a classe .active para este botao
+  button.classList.add("active")
+}
